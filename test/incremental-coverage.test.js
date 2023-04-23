31d2742b14889ab2600adb45af7d5ff5e47c4492
Jodeee <imjie@outlook.com>
2023-04-23 14:04:32 +0800

    chore: 收敛代码，支持直接获取增量覆盖率和增量摘要

--- test/incremental-coverage.test.js
+++ test/incremental-coverage.test.js
@@ -1,45 +1,35 @@
 'use strict';
 
 const assert = require('assert');
-const { 
-  incrementalStatements,
-  incrementalBranches,
-  incrementalFunctions 
-} = require('../lib/core/incremental-coverage');
-
+const incrementalCoverage = require('../lib/core/incremental-coverage');
 
 describe('./test/incremental-coverage.test.js', () => {
 
   it('incremental coverage should work', () => {
-    const coverageSummary = {};
+
     const diffMap = require('./fixture/test-coverage-diff.json');
     const coverageMap = require('./fixture/test-coverage-final.json');
 
-    for (const file in diffMap) {
-      const statements = incrementalStatements(coverageMap[file], diffMap[file]);
-      const branchs = incrementalBranches(coverageMap[file], diffMap[file]);
-      const functions = incrementalFunctions(coverageMap[file], diffMap[file]);
-      coverageSummary[file] = {
-        statements,
-        branchs,
-        functions,
-      };
-    }
+    const incrementalMap = incrementalCoverage(coverageMap, diffMap);
 
-    const test1 = coverageSummary['/root/demo/app/coverage_test1.js'];
-    assert.equal(test1.statements.cover, 11);
-    assert.equal(test1.statements.total, 12);
-    assert.equal(test1.branchs.cover, 2);
-    assert.equal(test1.branchs.total, 4);
-    assert.equal(test1.functions.cover, 4);
-    assert.equal(test1.functions.total, 6);
+    assert.equal(incrementalMap.coverage['/root/demo/app/coverage_test1.js'].s[1], 1);
+    assert.equal(incrementalMap.coverage['/root/demo/app/coverage_test2.js'], undefined);
+    assert.equal(incrementalMap.coverage['/root/demo/app/coverage_test3.js'], undefined);
 
-    const test2 = coverageSummary['/root/demo/app/coverage_test2.js'];
-    assert.equal(test2.statements.cover, 0);
-    assert.equal(test2.statements.total, 32);
-    assert.equal(test2.branchs.cover, 0);
-    assert.equal(test2.branchs.total, 0);
-    assert.equal(test2.functions.cover, 0);
-    assert.equal(test2.functions.total, 0);
+    const summary1 = incrementalMap.summary['/root/demo/app/coverage_test1.js'];
+    assert.equal(summary1.statements.cover, 11);
+    assert.equal(summary1.statements.total, 12);
+    assert.equal(summary1.branchs.cover, 2);
+    assert.equal(summary1.branchs.total, 4);
+    assert.equal(summary1.functions.cover, 4);
+    assert.equal(summary1.functions.total, 6);
+
+    const summary2 = incrementalMap.summary['/root/demo/app/coverage_test2.js'];
+    assert.equal(summary2.statements.cover, 0);
+    assert.equal(summary2.statements.total, 32);
+    assert.equal(summary2.branchs.cover, 0);
+    assert.equal(summary2.branchs.total, 0);
+    assert.equal(summary2.functions.cover, 0);
+    assert.equal(summary2.functions.total, 0);
   });
 });
