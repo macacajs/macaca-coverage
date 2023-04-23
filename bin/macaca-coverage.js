f7ffcb95f917d2baa1108ef829964666ba53e56e
Jodeee <imjie@outlook.com>
2023-04-23 14:50:16 +0800

    feat: 支持直接获取增量覆盖率和增量摘要

--- bin/macaca-coverage.js
+++ bin/macaca-coverage.js
@@ -7,6 +7,9 @@ const path = require('path');
 const EOL = require('os').EOL;
 const program = require('commander');
 const Coverage = require('..');
+const getDiff = require('../lib/core/diff');
+const summaryTemplate = require('../lib/template/summary-template');
+const { incrementalReporter } = require('../lib/core/incremental-coverage');
 
 const pkg = require('../package.json');
 const cwd = process.cwd();
@@ -14,37 +17,93 @@ const cwd = process.cwd();
 program
   .option('-f, --file <s>', 'coverage file')
   .option('-v, --versions', 'output version infomation')
-  .usage('');
+  .usage('')
+  .action(options => {
+    if (options.versions) {
+      console.info(`${EOL} ${pkg.version} ${EOL}`);
+      process.exit(0);
+    }
+
+    if (options.file) {
+      const {
+        collector,
+        Reporter,
+      } = Coverage({
+        runtime: 'web',
+      });
+      const coverageFile = path.resolve(program.file);
+      const __coverage__ = JSON.parse(fs.readFileSync(coverageFile, 'utf8'));
+      collector.add(__coverage__);
+      const distDir = path.join(cwd, `coverage_${Date.now()}`);
+      const reporter = new Reporter(null, distDir);
+      reporter.addAll([
+        'html',
+        'lcov',
+        'json',
+      ]);
+      reporter.write(collector, true, () => {
+        const coverageHtml = path.join(distDir, 'index.html');
+        console.log(coverageHtml);
+      });
+    } else {
+      program.help();
+      process.exit(0);
+    }
+  });
+
+program
+  .command('diff')
+  .description('Get diff')
+  .option('-c, --currentBranch [String]', 'current branch.', null)
+  .option('-t, --targetBranch [String]', 'target branch.', 'master')
+  .option('-o, --output [String]', 'diff save path.', 'coverage-diff.json')
+  .action(async (option) => {
+
+    const output = option.output;
+    const currentBranch = option.currentBranch;
+    const targetBranch = option.targetBranch;
+
+    const diffMap = await getDiff(cwd, currentBranch, targetBranch);
+    const newDiffMap = {};
+    for (const file in diffMap) {
+      const filePath = path.join(cwd, file);
+      newDiffMap[filePath] = diffMap[file];
+    }
+
+    console.info(`diff path: ${output}`);
+
+    fs.writeFileSync(output, JSON.stringify(newDiffMap, null, 2));
+  });
+
+program
+  .command('incremental-reporter')
+  .description('Get incremental reporter')
+  .option('-c, --coveragePath [String]', 'coverage path.', 'coverage/coverage-final.json')
+  .option('-d, --diffPath [String]', 'diff path.', 'coverage-diff.json')
+  .option('-o, --output [String]', 'reporter path.', 'coverage')
+  .action(async (option) => {
+
+    const output = path.join(cwd, option.output);
+    const summaryHtmlpath = path.join(output, 'summary.html');
+    const diffPath = path.join(cwd, option.diffPath);
+    const coveragePath = path.join(cwd, option.coveragePath);
+    console.info(`diff path: ${diffPath}`);
+    console.info(`coverage path: ${coveragePath}`);
+
+    const diffMap = require(diffPath);
+    const coverageMap = require(coveragePath);
+
+    const incrementalMap = incrementalReporter(coverageMap, diffMap, {
+      projectPath: cwd,
+      needCollectedIncludes: process.env.INCREMENTAL_NEED_COLLECTED_INCLUDES || [],
+      reporterPath: output,
+    });
+
+    console.info(incrementalMap);
+    console.info(`reporter: ${output}`);
+    console.info(`summary: ${summaryHtmlpath}`);
+    fs.writeFileSync(summaryHtmlpath, summaryTemplate(incrementalMap.reporterHtml));
+  });
 
 program.parse(process.argv);
 
-if (program.versions) {
-  console.info(`${EOL} ${pkg.version} ${EOL}`);
-  process.exit(0);
-}
-
-if (program.file) {
-  const {
-    collector,
-    Reporter
-  } = Coverage({
-    runtime: 'web'
-  });
-  const coverageFile = path.resolve(program.file);
-  const __coverage__ = JSON.parse(fs.readFileSync(coverageFile, 'utf8'));
-  collector.add(__coverage__);
-  const distDir = path.join(cwd, `coverage_${Date.now()}`);
-  const reporter = new Reporter(null, distDir);
-  reporter.addAll([
-    'html',
-    'lcov',
-    'json'
-  ]);
-  reporter.write(collector, true, () => {
-    const coverageHtml = path.join(distDir, 'index.html');
-    console.log(coverageHtml);
-  });
-} else {
-  program.help();
-  process.exit(0);
-}
---
+++ test/incremental-coverage.test.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const path = require('path');
+const assert = require('assert');
+const { incrementalReporter } = require('../lib/core/incremental-coverage');
+const cwd = process.cwd();
+
+describe('./test/incremental-coverage.test.js', () => {
+
+  const newDiffMap = {};
+  const newCoverageMap = {};
+  const test1 = path.join(cwd, 'test/fixture/report/test1.js');
+  const test2 = path.join(cwd, 'test/fixture/report/test2.js');
+  const test3 = path.join(cwd, 'test/fixture/report/test3.js');
+ 
+  before('before', () => {
+    const diffMap = require('./fixture/test-coverage-diff.json');
+    const coverageMap = require('./fixture/test-coverage-final.json');
+    
+    for (const file in diffMap) {
+      const filePath = path.join(cwd, file);
+      newDiffMap[filePath] = diffMap[file];
+    }
+
+    for (const file in coverageMap) {
+      const filePath = path.join(cwd, file);
+      newCoverageMap[filePath] = coverageMap[file];
+    }
+  })
+  it('incremental coverage should work', () => {
+    const incrementalMap = incrementalReporter(newCoverageMap, newDiffMap, {
+      projectPath: cwd,
+      reporterPath: path.join(cwd, 'test/fixture/report'),
+    });
+    assert.equal(incrementalMap.coverage[test1].s[1], 1);
+    assert.equal(incrementalMap.coverage[test2], undefined);
+    assert.equal(incrementalMap.coverage[test3], undefined);
+  });
+
+  it('incremental summary should work', () => {
+
+    let incrementalMap = incrementalReporter(newCoverageMap, newDiffMap, {
+      projectPath: cwd,
+      reporterPath: path.join(cwd, 'test/fixture/report'),
+    });
+    const summary1 = incrementalMap.summary[test1];
+    assert.equal(summary1.statements.cover, 11);
+    assert.equal(summary1.statements.total, 12);
+    assert.equal(summary1.branchs.cover, 2);
+    assert.equal(summary1.branchs.total, 4);
+    assert.equal(summary1.functions.cover, 4);
+    assert.equal(summary1.functions.total, 6);
+
+    const summary2 = incrementalMap.summary[test2];
+    assert.equal(summary2, undefined);
+
+    // needCollectedIncludes config
+    incrementalMap = incrementalReporter(newCoverageMap, newDiffMap, {
+      projectPath: process.cwd(),
+      needCollectedIncludes: ['**/test2.js'],
+      reporterPath: path.join(process.cwd(), 'test/fixture/report'),
+    });
+
+    const summary3 = incrementalMap.summary[test2];
+    assert.equal(summary3.statements.cover, 0);
+    assert.equal(summary3.statements.total, 32);
+    assert.equal(summary3.branchs.cover, 0);
+    assert.equal(summary3.branchs.total, 0);
+    assert.equal(summary3.functions.cover, 0);
+    assert.equal(summary3.functions.total, 0);
+
+  });
+
+  it('incremental reporter should work', () => {
+     const incrementalMap = incrementalReporter(newCoverageMap, newDiffMap, {
+      projectPath: process.cwd(),
+      needCollectedIncludes: ['**/test2.js', '**/test4.js'],
+      reporterPath: path.join(process.cwd(), 'test/fixture/report'),
+    });
+
+    const reg = /<td class="coverage-percent".*>\d+%<\/td>/g;
+    let list = incrementalMap.reporterHtml.match(reg) || [];
+    assert.equal(list.length, 3);
+    assert(list[0].includes('75%'));
+    assert(list[1].includes('92%'));
+    assert(list[2].includes('0%'));
+  });
+});
